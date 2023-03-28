2fdf5a65c3f992436d609f00e39810497b0a3bb5
Karla Magueta <k.magueta@gmail.com>
2023-03-28 16:11:25 +0100

    ajuste site

---
+++ aulas/Bikcraft/javascript.js
@@ -0,0 +1,8 @@
+function ('livro') {
+    const nome = ' O Senhor dos Anéis'
+    const ano = 1954
+    const autor = 'J. R. R. Tolkien'
+
+    console.log(nome.toLocaleUpperCase)
+    console.log (2023 - ano)
+}
