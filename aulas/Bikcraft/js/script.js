6f7a37c56694541244e0183abbcc26724d4ab444
Karla Magueta <k.magueta@gmail.com>
2023-04-13 12:05:37 +0100

    ajustando js

--- aulas/Bikcraft/js/script.js
+++ aulas/Bikcraft/js/script.js
@@ -1,16 +1,14 @@
-// Ativar links do menu
+
 
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link) {
-    const url = window.location.href
+    const url = window.location.href;
     const href = link.href;
     url.includes    
-    if (url.includes(href)){
-        link.classList.add('ativo')
+    if (url.includes(href)) {
+        link.classList.add("ativo")
     }
-
-    console.log(url)
 }
 
 links.forEach(ativarLink);
@@ -20,32 +18,29 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-    const elemento = document.getElementById(parametro)
-    elemento.checked = true
-        if (elemento) {
-            elemento.checked = true;
-        }
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
-parametros.forEach(ativarProduto)
-
-console.log(parametros);
-
 //Faq
+
 const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta(event){
-    const pergunta = event.currentTarget;
-    const controls = pergunta.getAttribute("aria-controls");
-    const resposta = document.getElementById(controls);
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
-    resposta.classList.toggle("ativa");
-    const ativa = resposta.classList.contains("ativa");
-    pergunta.setAttribute("aria-expanded", ativa)
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPerguntas(pergunta) {
-    pergunta.addEventListener("click", ativarPergunta )
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
@@ -55,13 +50,14 @@ perguntas.forEach(eventosPerguntas);
 const galeria = document.querySelectorAll('.bicicleta-img img')
 const galeriaContainer = document.querySelector('.bicicleta-img')
 
-function trocarImagem(event){
+function trocarImagem(event) {
     const img = event.currentTarget;
-    window.matchMedia('(min-width: 1000px)').matches;
-    if (media) {}
-        galeriaContainer.prepend(img)
+    const media = matchMedia("(min-width:1000px)").matches;
+  
+    if (media) {
+      galeriaContainer.prepend(img);
     }
-}
+  }
 
 function eventosGaleria(img){
     img.addEventListener('click', trocarImagem)
@@ -72,4 +68,4 @@ galeria.forEach(eventosGaleria);
 // Iniciando Plugin de Animação
 if (window.SimpleAnime){
     new SimpleAnime();
-}
+} 
